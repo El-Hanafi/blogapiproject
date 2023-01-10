dd705ec56558734b2251c83d3213a8138809e711
el-hanafi <m.h.benabbou@gmail.com>
2023-01-10 20:18:13 +0100

    added all models for comments Users and category and posts

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 
 
 // function to connect
-
 const dbConnect = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB connected successfully')
     } catch (error) { 
-
+        console.log(error.message);
+        process.exit(1)
     }
 }
+
+dbConnect();
---
+++ model/BlogUser/BlogUser.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+
+//create schema
+const BlogUserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+
+    lastname: {
+        type: String,
+    },
+    profilePhoto: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password name is required']
+    },
+    postCount: {
+        type: number,
+        default: 0,
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    },
+    role:{
+        type:String,
+        enum:['Admin','Guest','Editor']
+    },
+    viewedBy:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+],
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    active: {
+        type: Boolean,
+        default: true,
+    },
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        },
+    ]
+
+},{
+    timestamps: true
+});
+
+
+//Compile the bloguser model
+const BlogUser = mongoose.model('BlogUser', BlogUserSchema);
+
+module.exports= BlogUser;
